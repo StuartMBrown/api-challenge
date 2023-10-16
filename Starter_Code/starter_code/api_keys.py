35de9c490d161d2ee822d3a8867bcf1755f53c29
StuartMBrown <datautisticgamer@gmail.com>
2023-10-15 22:41:37 -0500

    What is being added/changed

--- Starter_Code/starter_code/api_keys.py
+++ Starter_Code/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key ="fb3d8263a6ae59fd1c79e4ec86a457eb"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key ="e0f7b6aef3f54603a6fbce94d88fb32c"
